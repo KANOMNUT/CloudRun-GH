91c27bbebcb81f50ca1d8d62419b6eb808aead33
Thanachai T <thanachai.t@7solutions.co.th>
2025-07-17 13:57:35 +0700

    feat: add /temp

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import healthz from './src/healthz.js';
 import readyz from './src/readyz.js';
 import request from './src/request.js';
+import temp from './src/temp.js'
 
 const app = express();
 app.disable('x-powered-by');
@@ -12,6 +13,7 @@ const port = process.env.PORT || 8080;
 app.use('/healthz', healthz);
 app.use('/readyz', readyz);
 app.use('/request', request);
+app.use('/temp', temp);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
